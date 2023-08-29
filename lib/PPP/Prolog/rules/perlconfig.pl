502493835f66d18ecb7a598256c61c32f7901c13
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-08-29 18:42:03 -0400

    Fix indentation

--- lib/PPP/Prolog/rules/perlconfig.pl
+++ lib/PPP/Prolog/rules/perlconfig.pl
@@ -11,20 +11,20 @@
 :- dynamic perl_config_data/1.
 
 read_perl_config_dump_terms(PerlPath, Terms) :-
-        setup_call_cleanup(
-            process_create(
-                PerlPath, [
-                        '-Mlib::projectroot=lib',
-                        '-MPPP::Prolog::DumpConfig=dump_terms',
-                        '-e', 'dump_terms'
-                    ],
-                    [ stdout(pipe(Out)) ]),
-            parse_perl_config_as_terms(Out, Terms),
-            close(Out)).
+    setup_call_cleanup(
+        process_create(
+            PerlPath, [
+                '-Mlib::projectroot=lib',
+                '-MPPP::Prolog::DumpConfig=dump_terms',
+                '-e', 'dump_terms'
+            ],
+            [ stdout(pipe(Out)) ]),
+        parse_perl_config_as_terms(Out, Terms),
+        close(Out)).
 
 parse_perl_config_as_terms(Out, Terms) :-
-            read_stream_to_codes(Out, Codes),
-            read_term_from_codes(Codes, Terms, [character_escapes(true)] ).
+    read_stream_to_codes(Out, Codes),
+    read_term_from_codes(Codes, Terms, [character_escapes(true)] ).
 
 assert_perl_config_data :-
     read_perl_config_dump_terms(path(perl),Terms), assertz( perl_config_data(Terms) ).
